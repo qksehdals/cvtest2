de088103f52d9278ceccc184994252ed84e794a5
qksehdals <qksehdals@naver.com>
2015-12-30 16:12:33 +0900

    props 추가
    라이브러리/문서/visual studio 2013 폴더에 넣을것.

--- cvtest.cpp
+++ cvtest.cpp
@@ -2,7 +2,6 @@
 //#include "opencv/cxcore.h"
 #include "opencv/cv.h"
 #include "opencv2/imgproc/imgproc.hpp"
-
 #include "opencv2/core/core.hpp"
 #include "opencv2/nonfree/features2d.hpp"
 
@@ -14,23 +13,29 @@ void main(){
 	std::string str;
 	str = "c:/cvimagetest/test_img_org.png";
 
+	//load image
 	Mat image;
 	image = cv::imread(str, CV_LOAD_IMAGE_COLOR);
 
+	//exeption handling
 	if (image.empty()){
 		cout << "파일 읽기 실패" << endl;
 	}
 
+
+	//apply Canny
 	Mat canny;
 	Canny(image, canny, 50, 100);
 
+	//sift image descriptor
 	SiftDescriptorExtractor detector;
 	vector<KeyPoint> keypoints;
 	detector.detect(canny, keypoints);
 	drawKeypoints(canny, keypoints, canny);
 
+	//match features
 	BFMatcher matcher;
-	int a = 123;
+
 	//matcher.match()
 
 
